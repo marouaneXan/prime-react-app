45f05ad2bdeb3235d3ae7214077a8d4ed28c99fd
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-25 12:58:54 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Menubar } from 'primereact/menubar';
+function App() {
+  return (
+    <div className="App">
+      <Menubar className='bg-blue-600 p-3 text-2xl' start={'E-commerce'} />
+    </div>
+  );
+}
+
+export default App;
